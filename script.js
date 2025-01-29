f8e5c98fc660088c6915cee0377b14cd7cd5a6e0
Breadipsy <lovejing@BreadipsydeMacBook-Air.local>
2025-01-29 12:59:17 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// 平滑滚动到不同部分
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
